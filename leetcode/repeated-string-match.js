e3b57069f52e9a5f9e109b301e31793c52cbc91f
sairina <sairina.merinotsui@gmail.com>
2020-05-10 22:40:15 -0700

    updated ternary

--- leetcode/repeated-string-match.js
+++ leetcode/repeated-string-match.js
@@ -7,11 +7,5 @@ var repeatedStringMatch = (A, B) => {
   let count = Math.ceil(B.length / A.length);
   let str = A.repeat(count);
   
-  if (str.includes(B)) {
-    return count;
-  } else if ((str + A).includes(B)) {
-    return count + 1;
-  } else {
-    return -1;
-  }
+  return str.includes(B) ? count : (str + A).includes(B) ? count + 1 : -1;
 };
