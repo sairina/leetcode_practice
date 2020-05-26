3fa1615b8e613100ac6ac746afe65ed4461ee013
sairina <sairina.merinotsui@gmail.com>
2020-05-26 11:30:45 -0700

    updated code

--- leetcode/remove-duplicates.js
+++ leetcode/remove-duplicates.js
@@ -1,13 +1,42 @@
-var removeDuplicates = function(nums) {
-  let left = 0;
+// var removeDuplicates = function(nums) {
+//   let left = 0;
   
-  for(let right = 1; right < nums.length; right++){
-    if (nums[left] !== nums[right]) { //if not equal, increment left; set left equal to right val
+//   for(let right = 1; right < nums.length; right++){
+//     if (nums[left] !== nums[right]) { //if not equal, increment left; set left equal to right val
+//       left++;
+//       nums[left] = nums[right];
+//     }
+//     // otherwise, keep going through array length
+//   }
+//   return left + 1;
+  
+// };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var removeDuplicates = function(nums) {
+  // create left and right pointers
+  // while right is less than length of array
+    // if left pointer is not equal right pointer
+      // increase left
+      // set left element to what the right is
+    // increase right
+  
+  //return nums
+  
+  let left = 0;
+  let right = 1;
+  
+  while (right < nums.length) {
+    if (nums[left] !== nums[right]) {
       left++;
       nums[left] = nums[right];
     }
-    // otherwise, keep going through array length
+    right++;
   }
-  return left + 1;
   
-};
+  return left + 1;
+}
