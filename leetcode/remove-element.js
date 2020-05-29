5455200c29c006b27b531295977418f52eefc020
sairina <sairina.merinotsui@gmail.com>
2020-05-28 21:54:08 -0700

    refactored with splice

--- leetcode/remove-element.js
+++ leetcode/remove-element.js
@@ -1,24 +1,44 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} val
+//  * @return {number}
+//  */
+// var removeElement = function(nums, val) {
+//   // left pointer at 0
+  
+//   // fast pointer iterates over array starting at 0
+//     // if fast pointer is not val
+//       // swap
+//       //increase left pointer
+  
+//   let left = 0;
+  
+//   for (let right = 0; right < nums.length; right++) {
+//     if (nums[right] !== val) {
+//       [nums[right], nums[left]] = [nums[left], nums[right]];
+//       left++;
+//     }
+//   }
+  
+//   return left;
+// };
+
 /**
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
  */
 var removeElement = function(nums, val) {
-  // left pointer at 0
+  // iterate over nums backwards
+    // if element is val
+      // splice
+  // return length
   
-  // fast pointer iterates over array starting at 0
-    // if fast pointer is not val
-      // swap
-      //increase left pointer
-  
-  let left = 0;
-  
-  for (let right = 0; right < nums.length; right++) {
-    if (nums[right] !== val) {
-      [nums[right], nums[left]] = [nums[left], nums[right]];
-      left++;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
     }
   }
   
-  return left;
+  return nums.length;
 };
