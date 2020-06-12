f883dece2c35dab4905d4ad531151dee5bec834a
sairina <sairina.merinotsui@gmail.com>
2020-06-11 21:32:45 -0700

    added interview-cake dir

---
+++ interview-cake/permutation-palindrome.js
@@ -0,0 +1,11 @@
+// Write an efficient function that checks whether any permutation ↴ of an input string is a palindrome. ↴
+
+// You can assume the input string only contains lowercase letters.
+
+// Examples:
+
+// "civic" should return true
+// "ivicc" should return true
+// "civil" should return false
+// "livci" should return false
+
