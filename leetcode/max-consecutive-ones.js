498c344dde5f6ea92b88f8c8e3cd53919ee2937f
sairina <sairina.merinotsui@gmail.com>
2020-06-21 23:06:48 -0700

    added code

---
+++ leetcode/max-consecutive-ones.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+  //set count var
+  //set maxCount var
+  //iterate through array
+    //if element is 1
+      //add to count
+        //if count is greater than maxCount
+          //maxCount set to count
+    //else
+      //reset count to 0
+  //return maxCount
+  
+  let count = 0;
+  let maxCount = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      count++;
+      if (count > maxCount) {
+        maxCount = count;
+      }
+    } else {
+      count = 0;
+    }
+  }
+  return maxCount;
+};
