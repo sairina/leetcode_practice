d1b16dec5a75c56d78715a47bde8bf0cd810cf9a
sairina <sairina.merinotsui@gmail.com>
2020-06-29 22:44:36 -0700

    refactored to ES6

--- hacker-rank/camelcase.js
+++ hacker-rank/camelcase.js
@@ -34,11 +34,19 @@ function camelcase(s) {
 
     let count = 1;
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === s[i].toUpperCase()) {
+    // for (let i = 0; i < s.length; i++) {
+    //     if (s[i] === s[i].toUpperCase()) {
+    //         count += 1;
+    //     }
+    // }
+    
+    for (let letter of s) {
+        if (letter === letter.toUpperCase()) {
             count += 1;
         }
     }
+
+
     return count;
 
 }
