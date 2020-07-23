e8e38b65b1c6750aa273b1cba77bd382902d6c93
sairina <sairina.merinotsui@gmail.com>
2020-07-22 22:51:51 -0700

    refactor

--- leetcode/best-time-to-buy-and-sell-stock.js
+++ leetcode/best-time-to-buy-and-sell-stock.js
@@ -1,24 +1,44 @@
+// /**
+//  * @param {number[]} prices
+//  * @return {number}
+//  */
+// var maxProfit = function(prices) {
+  
+//   let maxProfit = 0;
+//   let left = 0;
+//   let right = 1;
+  
+//   while (right <= prices.length) {
+//     if (prices[right] > prices[left]) {
+      
+//       let profit = prices[right] - prices[left];
+//       maxProfit = Math.max(profit, maxProfit);
+      
+//       right++;
+      
+//     } else {
+//       left++;
+//       right = left + 1;
+//     }
+//   }
+  
+//   return maxProfit;
+// };
+
 /**
  * @param {number[]} prices
  * @return {number}
  */
 var maxProfit = function(prices) {
-  
   let maxProfit = 0;
-  let left = 0;
-  let right = 1;
+  let min = Infinity;
   
-  while (right <= prices.length) {
-    if (prices[right] > prices[left]) {
-      
-      let profit = prices[right] - prices[left];
-      maxProfit = Math.max(profit, maxProfit);
-      
-      right++;
-      
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < min) {
+      min = prices[i];
     } else {
-      left++;
-      right = left + 1;
+      let profit = prices[i] - min;
+      maxProfit = Math.max(maxProfit, profit);
     }
   }
   
