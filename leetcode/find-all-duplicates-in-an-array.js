b93a34cdd46266cfe5c26c51bad33505ccc54ead
sairina <sairina.merinotsui@gmail.com>
2020-08-06 12:14:48 -0700

    refactor multiple pointers

--- leetcode/find-all-duplicates-in-an-array.js
+++ leetcode/find-all-duplicates-in-an-array.js
@@ -3,12 +3,20 @@
  * @return {number[]}
  */
 var findDuplicates = function(nums) {
-  let dup = [];
+  let duplicates = [];
   let sorted = nums.sort((a,b) => a - b);
-  for (let i = 0; i < sorted.length; i++) {
-    if (sorted[i] === sorted[i + 1]) {
-      dup.push(sorted[i]);
+  let left = 0;
+  let right = 1;
+  
+  while (right < sorted.length) {
+    if (sorted[left] !== sorted[right]) {
+      left++;
+      right++;
+    } else {
+      duplicates.push(sorted[left]);
+      left += 2;
+      right += 2;
     }
   }
-  return dup;
+  return duplicates;
 };
