cfb0063576d95bc54ed4b40a9ab02d464d2e34b0
sairina <sairina.merinotsui@gmail.com>
2020-08-06 12:17:21 -0700

    refactor object hashmap

--- leetcode/find-all-duplicates-in-an-array.js
+++ leetcode/find-all-duplicates-in-an-array.js
@@ -3,20 +3,18 @@
  * @return {number[]}
  */
 var findDuplicates = function(nums) {
+
   let duplicates = [];
-  let sorted = nums.sort((a,b) => a - b);
-  let left = 0;
-  let right = 1;
+  let numObj = {};
+  for (let num of nums) {
+    numObj[num] = numObj[num] + 1 || 1;
+  }
   
-  while (right < sorted.length) {
-    if (sorted[left] !== sorted[right]) {
-      left++;
-      right++;
-    } else {
-      duplicates.push(sorted[left]);
-      left += 2;
-      right += 2;
+  for (let key in numObj) {
+    if (numObj[key] === 2) {
+      duplicates.push(key);
     }
   }
+  
   return duplicates;
 };
